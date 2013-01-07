882c6ce7cd3d8a55c2ddc0b2e017ab3d20e02bc1
Julien Castelain <jcastelain@gmail.com>
2013-01-07 18:27:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+dart events
+===========
+
+A simple event system for [Dart](http://www.dartlang.org) based on
+existing implementations done in JavaScript or ActionScript3.
+
+*Available at the Dart [pub](http://pub.dartlang.org)*
+
---
+++ web/example_basic.dart
@@ -0,0 +1,38 @@
+import 'dart:html';
+import 'package:/unittest/unittest.dart';
+import '../lib/dart_events.dart';
+
+
+void handler(List<dynamic> a) {
+  args = a;
+}
+
+List<dynamic> args;
+
+void main() {
+
+  EventEmitter emitter = new EventEmitter();
+
+  test('it only add the same Function once', () {
+    emitter.on('test', handler);
+    emitter.on('test', handler);
+    expect(emitter.listeners().length, equals(1));
+  });
+
+  test('it should remove a Function if it exists', () {
+    emitter.off('NON_EXISTING', handler);
+    expect(emitter.listeners().length, equals(1));
+  });
+
+  test('''
+it should call a listener Function 
+if a type of "event" is emitted and 
+is associtated to this type's listeners
+''', () {
+    emitter.emit('test', [1, 2, 3]);
+    expect(args, equals([1, 2, 3]));
+  });
+
+}
+
+
