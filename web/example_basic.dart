652cc203b255f8e1ead3f43eec9e11714f550e52
Julien Castelain <jcastelain@gmail.com>
2013-01-07 19:35:19 +0100

    updated the example removed unnecessary code

--- web/example_basic.dart
+++ web/example_basic.dart
@@ -2,16 +2,11 @@ import 'dart:html';
 import '../lib/dart_events.dart';
 
 
-void handler(List<dynamic> a) {
-  args = a;
-  window.alert(args.toString());
-}
-
-List<dynamic> args;
-
 void main() {
   EventEmitter emitter = new EventEmitter();
-  emitter.on('test', handler);
+  emitter.on('test', (List<dynamic> args) {
+      window.alert(args.toString());
+  });
 
   ButtonElement btn = new ButtonElement()
     ..text = 'Click me!'
